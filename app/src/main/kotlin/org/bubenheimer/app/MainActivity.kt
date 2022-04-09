5e4ac4fec348ddaaea3be33fa7a7269810fab224
Uli Bubenheimer <bubenheimer@users.noreply.github.com>
2022-04-09 13:57:31 -0400

    Initial commit

---
+++ app/src/main/kotlin/org/bubenheimer/app/MainActivity.kt
@@ -0,0 +1,26 @@
+package org.bubenheimer.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.Slider
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            var value: Float by remember { mutableStateOf(0f) }
+
+            Slider(
+                    value,
+                    onValueChange = { value = it },
+                    onValueChangeFinished = { println("Value change finished") }
+            )
+        }
+    }
+}
